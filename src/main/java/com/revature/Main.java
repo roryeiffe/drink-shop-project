13e2866cc40f93e397908d50fecc03c93eee851b
RoryEiffe <roryeiffe8@gmail.com>
2023-11-13 17:29:29 -0500

    Used AI to generate code to start server

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -1,8 +1,18 @@
 package com.revature;
 
+import com.revature.controller.ProductController;
+import com.sun.net.httpserver.HttpServer;
+
 // create a main class with empty main method:
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // set up the HttpServer to use ProductController on port 8080:
+        // AI GENERATED CODE:
+        // HttpServer server = HttpServer.create();
+
+        // server.bind(new ProductController(new ProductService()), 8080);
+
+        // server.start();
+
     }
 }
